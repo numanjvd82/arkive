aeb3ad8c174cbaf32139cee942e704af3aade56d
Numan Javed <numanjaved2001@gmail.com>
2025-12-22 23:42:10 +0500

    task: add basic web templates

--- core/router/router.go
+++ core/router/router.go
@@ -10,6 +10,14 @@ import (
 func New(db database.PgExecutor) *gin.Engine {
 	r := gin.Default()
 
+	r.LoadHTMLGlob("web/templates/*.html")
+	r.Static("/statics", "./web/statics")
+	r.GET("/", handlers.WebHome())
+	r.GET("/login", handlers.WebLoginGet())
+	r.POST("/login", handlers.WebLoginPost())
+	r.GET("/signup", handlers.WebSignupGet())
+	r.POST("/signup", handlers.WebSignupPost())
+
 	api := r.Group("/api")
 	{
 		api.GET("/health", handlers.Health(db))
