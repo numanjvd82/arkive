c64c170952aaaa59c188155c0547021cf1400d73
Numan Javed <numanjaved2001@gmail.com>
2026-01-04 03:14:36 +0500

    Add Monetag tag to dashboard

---
+++ core/web/static/monetag-push-ad.js
@@ -0,0 +1,11 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://3nbf4.com/act/files/tag.min.js?z=10414558";
+  script.async = true;
+  script.setAttribute("data-cfasync", "false");
+
+  const head = document.head || document.documentElement;
+  if (head) {
+    head.appendChild(script);
+  }
+})();
